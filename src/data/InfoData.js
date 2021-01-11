4829033497a02660c2d1a7d0ae3f7facd8fb53ba
nicolanzi1 <nicolanzi@gmail.com>
2021-01-11 19:16:12 +0000

    Built the InfoSection component and wrote its different elements whilst styling them with styled-components. Also built an InfoData.js file to be able to change specific sections and info, including imported images

---
+++ src/data/InfoData.js
@@ -0,0 +1,25 @@
+import ImageOne from '../images/house-6.jpg'
+import ImageTwo from '../images/house-7.jpg'
+
+export const InfoData = {
+  heading: 'Explore our beautiful homes',
+  paragraphOne:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda in eius molestiae molestias sapiente amet veritatis odit facere labore aliquid quidem dolorum libero, sequi hic doloremque numquam. Ducimus, perferendis libero.',
+  paragraphTwo:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda in eius molestiae molestias sapiente amet veritatis odit facere labore aliquid quidem dolorum libero, sequi hic doloremque numquam.',
+  buttonLabel: 'View Homes',
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+}
+
+export const InfoDataTwo = {
+  heading: 'Modern Designs',
+  paragraphOne:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda in eius molestiae molestias sapiente amet veritatis odit facere labore aliquid quidem dolorum libero.',
+  paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  buttonLabel: 'View Homes',
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+}
